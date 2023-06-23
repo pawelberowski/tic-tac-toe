476c1a8707d5f27161d57f0d51c6d543a06a8303
Taz <pawelberowski@gmail.com>
2023-06-23 16:36:33 +0200

    Created start button that switches pages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './styles.css';
 
 console.log("It's Working");
+
+const startButton = document.querySelector('#start-button');
+const board = document.querySelector('.board-container');
+const nameInputsContainer = document.querySelector('.name-inputs-container');
+
+if (startButton && board && nameInputsContainer) {
+  startButton.addEventListener('click', function () {
+    nameInputsContainer.classList.add('hidden');
+    board.classList.remove('hidden');
+  });
+}
