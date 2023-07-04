8611474e92626f005c7d8298606c8ac146689bdb
Taz <pawelberowski@gmail.com>
2023-07-04 18:26:44 +0200

    Added current-shape paragraph to html to fix bugs after restart

--- src/fillCell.js
+++ src/fillCell.js
@@ -1,18 +1,18 @@
 import { infoDisplay } from './index';
 import { checkScore } from './checkScore';
 
-let currentShape = 'circle';
+export const currentShape = document.querySelector('.current-shape');
 export function fillCell(event) {
   const cellContent = document.createElement('div');
   const circlePlayerName = document.querySelector('#circle-input').value;
   const crossPlayerName = document.querySelector('#cross-input').value;
-  cellContent.classList.add(currentShape);
+  cellContent.classList.add(currentShape.innerHTML);
   event.target.append(cellContent);
-  if (currentShape === 'circle') {
-    currentShape = 'cross';
+  if (currentShape.innerHTML === 'circle') {
+    currentShape.innerHTML = 'cross';
     infoDisplay.textContent = "It's " + crossPlayerName + "'s Turn";
   } else {
-    currentShape = 'circle';
+    currentShape.innerHTML = 'circle';
     infoDisplay.textContent = "It's " + circlePlayerName + "'s Turn";
   }
   event.target.removeEventListener('click', fillCell);
--- src/restartGame.js
+++ src/restartGame.js
@@ -4,10 +4,12 @@ import {
   board,
   restartButton,
 } from './index';
+import { currentShape } from './fillCell';
 
 export function restartGame() {
   board.classList.add('hidden');
   board.innerHTML = '';
+  currentShape.innerHTML = 'circle';
   restartButton.classList.add('hidden');
   infoDisplay.classList.add('hidden');
   nameInputsContainer.classList.remove('hidden');
